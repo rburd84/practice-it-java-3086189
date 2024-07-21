b191940116f9b727a5e2bd889482f6a98162233b
rburd84 <61717752+rburd84@users.noreply.github.com>
2024-07-21 01:57:06 +0000

    exercise complete

--- _01_05b/MenuBuilder.java
+++ _01_05b/MenuBuilder.java
@@ -13,7 +13,7 @@ public class MenuBuilder {
     System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
-    ArrayList menu = new ArrayList();
+    ArrayList<MenuItem> menu = new ArrayList<>();
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
